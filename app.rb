6753ed2a6e25f4013a134371fb928a07518bef67
Sergey <me@serge.xyz>
2017-11-24 17:26:29 +0300

    minor editions

--- app.rb
+++ app.rb
@@ -1,21 +1,16 @@
 require "sinatra"
 require "sqlite3"
 
+@@db = SQLite3::Database.new "data.db"
+
+Dir['./controllers/**/*.rb'].each { |file| require file }
+
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
-	@PAGE_TITLE = "Главная"
+	@criteria = criteria
+	@PAGE_TITLE = "Центр занятости"
 	erb :index
-end
-
-get '/by_profession' do
-	@PAGE_TITLE = "Поиск по профессии"
-	erb :by_profession
-end
-
-get '/by_edu' do
-	@PAGE_TITLE = "Поиск по месту учебы"
-	erb :by_edu
 end
 
